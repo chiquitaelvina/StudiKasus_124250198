4991e27b46d6d62bb9ee2aba74118c2894c4c8ed
chiquita el <chiquitaelvina3@gmail.com>
2026-02-27 15:56:30 +0700

    Add files via upload

---
+++ studikasus_scr_124250198.cpp
@@ -0,0 +1,116 @@
+#include <iostream>
+using namespace std;
+
+struct Buku {
+	int no;
+	string nama;
+	int harga;
+	};
+	
+	void SequentialTanpaSentinel (Buku daftar [], int n, int cari) { 
+		bool found = false;
+		for (int i=0; i<n; i++) {
+			if (daftar[i].no == cari) {
+		cout << " Buku " << daftar [i].nama << " Ditemukan" << endl;
+		cout << " Harga: Rp. " <<daftar[i].harga << endl;
+		found = true;
+		}
+	}
+	if (!found) { 
+		cout << "Buku nomor " << cari << " tidak ditemukan" <<endl;
+			}
+
+		}
+		
+	  void SequentialSentinel (Buku daftar [], int n, int cari) {
+		  int i = 0;
+		  Buku terakhir = daftar[n-1]; 
+		  daftar[n-1].no = cari; // untuk sentinel
+		  while (daftar[i].no != cari) {
+			  i++;
+		  }
+		  
+		  daftar[n-1] = terakhir;
+		  if ( i < n-1 || daftar[n-1].no == cari) {
+			  cout << "Buku" << daftar[i].nama << " Ditemukan" << endl;
+			  cout << "Harga: Rp. " << daftar[i].harga << endl;
+		  } else {
+			  cout << "Buku nomor " << cari << " tidak ditemukan"	<< endl;
+		  }
+	  }	  
+		void Binary (Buku daftar[], int n, int cari ) {
+			int i=0;
+			int j= n-1;
+			bool found = false;
+			
+			while (i<= j) {
+				int k = (i+j) / 2; 
+					if (daftar[k].no == cari) {
+						cout << "Buku " << daftar[k].nama << " Ditemukan" << endl;
+						cout << "Harga: Rp." << daftar[k].harga << endl;
+						found = true;
+						break;
+						}
+						else if (daftar[k].no <cari) { 
+							i= k+1;
+							 }
+							
+							else {
+								j= k-1;
+							
+							 }
+								}
+							
+			
+		if (!found) {
+			cout << "Buku nomor "<< cari << "tidak ditemukan";
+		}
+	}
+		
+	int main () {
+		int jmlbuku = 5;
+		Buku daftar[5] = {
+			{1, "Tembus UGM", 75000},
+			{2, "Bela Negara", 80000},
+			{3, "Cinta UPNYK", 79000},
+			{4, "Cinta Mantan", 90000},
+			{5, "UTBK 2026", 62000}
+		};
+		
+		cout << " \tDAFTAR BUKU\t "<< endl;
+		for (int i=0; i < jmlbuku; i++) {
+			cout << daftar[i].no << ". " << daftar[i].nama<<endl;
+		}
+		
+		int nomorCari;
+		int pilih;
+		
+		cout << "Masukkan nomor buku yang dicari: ";
+		cin >> nomorCari;
+		
+		cout << "Metode pencarian: " <<endl;
+		cout << "1. Pencarian Sequential Tanpa Sentinel" <<endl;
+		cout << "2. Pencarian Sequential Sentinel" << endl;
+		cout << "3. Pencarian Binary " << endl;
+		cout << "Pilih pencarian : ";
+		cin >> pilih;
+		
+			if (pilih == 1) {
+				SequentialTanpaSentinel (daftar, jmlbuku, nomorCari);
+			} else if (pilih ==2) {
+				SequentialSentinel(daftar, jmlbuku, nomorCari);
+			} else if (pilih == 3) {
+				Binary (daftar, jmlbuku, nomorCari) ;
+			} else {
+				cout << " Tidak ada" << endl;
+				
+			}
+		
+		}
+		
+					
+					
+					
+					
+
+					
